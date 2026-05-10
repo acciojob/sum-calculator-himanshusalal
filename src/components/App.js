02dcfc6e8af6e40f2174a374d2eabab24e7dd323
himanshusalal <himanshusalal517@gmail.com>
2026-05-10 15:40:09 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [numbers, setNumbers] = useState([]);
   const [sum, setSum] = useState(0);
 
   const handleChange = (e) => {
-    const value = e.target.value;
-    setInput(value);
+    const value = parseInt(e.target.value);
 
-    if (value !== "") {
-      const num = parseInt(value);
-      setNumbers([num]);
+    if (!isNaN(value)) {
+      setSum(value);
     } else {
-      setNumbers([]);
+      setSum(0);
     }
   };
-
-  useEffect(() => {
-    const total = numbers.reduce((acc, curr) => acc + curr, 0);
-    setSum(total);
-  }, [numbers]);
 
   return (
     <div>
       {/* Do not remove the main div */}
 
-      
-           <h1>Sum Calculator</h1>
-           <input
-        type="number"
-        value={input}
-        onChange={handleChange}
-      />
+      <h1>Sum Calculator</h1>
+
+      <input type="number" onChange={handleChange} />
+
       <p>Sum: {sum}</p>
     </div>
   );
 };
 
-export default App;
+export default App
